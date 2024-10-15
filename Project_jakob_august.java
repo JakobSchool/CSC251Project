00fccf42732659126a844f3311a23fd3bb4e9e05
Marbiox <jaugust8@lab.cpcc.edu>
2024-10-15 15:54:33 -0400

    Added documentation comments

--- Project_jakob_august.java
+++ Project_jakob_august.java
@@ -29,16 +29,6 @@ public class Project_jakob_august {
       Policy policy = new Policy(policyNumber, providerName, firstName, lastName, age, smokingStatus, height, weight);
       
       //print policy data to the console
-      System.out.println("\n-----------------------------------------------------------");
-      System.out.println("\nPolicy Number: " + policy.getPolicyNumber());
-      System.out.println("\nProvider Name: " + policy.getProviderName());
-      System.out.println("\nPolicyholder's First Name: " + policy.getFirstName());
-      System.out.println("\nPolicyholder's Last Name: " + policy.getLastName());
-      System.out.println("\nPolicyholder's Age: " + policy.getAge());
-      System.out.println("\nPolicyholder's Smoking Status: " + policy.getSmokingStatus());
-      System.out.printf("\nPolicyholder's Height: %.1f inches", policy.getHeight());
-      System.out.printf("\n\nPolicyholder's Weight: %.1f pounds", policy.getWeight());
-      System.out.printf("\n\nPolicyholder's BMI: %.2f", policy.calculateBMI());
-      System.out.printf("\n\nPolicy Price: $%.2f", policy.calculatePrice());
+      System.out.println(policy);
    }
 }
